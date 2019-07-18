bfb66ebfd49a1d9dcc0aa0842d050547d7c9681e
Name <alexac@student.ntnu.no>
2019-07-18 23:20:52 +0200

    Issue 3 works for control number, not other numbers.

---
+++ 3-Issue_Factorial/src/app/App.java
@@ -0,0 +1,27 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int ans = 0;
+        double num = 600851475143D;
+        Double limSearchD = Math.ceil(Math.sqrt(num));
+        int limSearch = limSearchD.intValue();
+
+        for(int i = limSearch; i > 0; i--){
+            if(num%i == 0){ //Checks if it's a factorial of the number
+                //Check if the factorial is a prime number
+                boolean primeBool = true;
+                for(int u = 2; u < 9; u++){
+                    if(i%u == 0){
+                        primeBool = false;
+                    }
+                }
+                if(primeBool){ //If the factorial is also a prime number, answer found.
+                    ans = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
