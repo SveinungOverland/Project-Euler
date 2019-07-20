da06d4cad553695ac1fb185a38491a37f5f07c5a
Name <alexac@student.ntnu.no>
2019-07-20 21:34:23 +0200

    Issue 7 solved

---
+++ 7-Issue_10001st_prime/src/app/App.java
@@ -0,0 +1,26 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        long[] primes = new long[10001];
+        int primesFound = 0;
+        long currentNum = 2;
+        do{
+            boolean isPrime = true;
+            for(int i = 2; i < currentNum-1; i++){
+                if(currentNum%i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                primes[primesFound] = currentNum;
+                primesFound++;
+            }
+            currentNum++;
+        } while(primesFound < primes.length);
+        
+        System.out.println(primes[10000]);
+    }
+}
